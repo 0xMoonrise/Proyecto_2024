9403a0fc42f3d67ef3c24c9826053bf2d455eb55
AndyMasterChief <aela121090@gmail.com>
2024-10-29 18:33:24 -0700

    Agregado nuevo punto de interes

--- app.py
+++ app.py
@@ -8,7 +8,8 @@ mapa = folium.Map(location=[24.1426, -110.3128], zoom_start=14)
 puntos_interes = [
     {"name": "Punto de interes 1", "location": [24.144370, -110.312590]},
     {"name": "Punto de interes 2", "location": [24.142456, -110.313708]},
-    {"name": "Punto de interes 3", "location": [24.132456, -110.313708]}
+    {"name": "Punto de interes 3", "location": [24.132456, -110.313708]},
+    {"name": "Punto de interes 4", "location": [24.132356, -110.312708]}
 ]
 
 for punto in puntos_interes:
@@ -19,4 +20,3 @@ for punto in puntos_interes:
     ).add_to(mapa)
 
 st_folium(mapa, width=725)
-#test
